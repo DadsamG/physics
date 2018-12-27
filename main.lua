591d54ed90ece3d19154998e970c01c5a2d8141b
4v0v <4vZEROv@gmail.com>
2018-12-27 01:37:03 +0100

    update

--- main.lua
+++ main.lua
@@ -4,46 +4,48 @@ require("run")
 
 
 local world = love.physics.newWorld()
-world:setGravity(0, 1000)
-world:setCallbacks(
-    function(a, b, coll) print(a:getBody():getUserData()) end, -- begin
-    function(a, b, coll) end, -- end 
-    function(a, b, coll) end, -- pre
-    function(a, b, coll, normalimpulse1, tangentimpulse1, normalimpulse2, tangentimpulse2, ...) end -- post
-)
+    world:setGravity(0, 1000)
+    world:setCallbacks(
+        function(a, b, coll) print(a:getBody():getUserData()) end, -- begin
+        function(a, b, coll) end, -- end 
+        function(a, b, coll) end, -- pre
+        function(a, b, coll, normalimpulse1, tangentimpulse1, normalimpulse2, tangentimpulse2, ...) end -- post
+    )
 
 
 local ground = {}
-ground.body = love.physics.newBody(world, lg.getWidth()/2, lg.getHeight()) 
-ground.shape = love.physics.newRectangleShape(800, 20)
-ground.shape2 = love.physics.newRectangleShape(200, 0,20, 100, 20)
-ground.fixture = love.physics.newFixture(ground.body, ground.shape)
-ground.fixture2 = love.physics.newFixture(ground.body, ground.shape2)
-ground.body:setUserData("ground")
+    ground.body = love.physics.newBody(world, lg.getWidth()/2, lg.getHeight()) 
+    ground.shape = love.physics.newRectangleShape(800, 20)
+    ground.shape2 = love.physics.newRectangleShape(200, 0,20, 100, 20)
+    ground.fixture = love.physics.newFixture(ground.body, ground.shape)
+    ground.fixture2 = love.physics.newFixture(ground.body, ground.shape2)
+    ground.body:setUserData("ground")
 
 local ball = {}
-ball.body = love.physics.newBody(world, 650/2, 650/2, "dynamic")
-ball.shape = love.physics.newCircleShape( 20)
-ball.fixture = love.physics.newFixture(ball.body, ball.shape, 1)
-ball.fixture:setRestitution(0.9)
-ball.body:setUserData("ball")
+    ball.body = love.physics.newBody(world, 650/2, 650/2, "dynamic")
+    ball.shape = love.physics.newCircleShape( 20)
+    ball.fixture = love.physics.newFixture(ball.body, ball.shape, 1)
+    ball.fixture:setRestitution(0.9)
+    ball.body:setUserData("ball")
 
 local block = {}
-block.body = love.physics.newBody(world, 200, 550, "dynamic")
-block.shape = love.physics.newRectangleShape(0, 0, 50, 100)
-block.fixture = love.physics.newFixture(block.body, block.shape, 5)
+    block.body = love.physics.newBody(world, 200, 550, "dynamic")
+    block.shape = love.physics.newRectangleShape(0, 0, 50, 100)
+    block.fixture = love.physics.newFixture(block.body, block.shape, 5)
 
+-------------------------------
+--  <°)))>< <°)))>< <°)))><  --
+-------------------------------
 
 function love.update(dt)
-    world:update(dt)
-
     if down("right") then ball.body:applyForce( 1000, 0) end
     if down("left" ) then ball.body:applyForce(-1000, 0) end
     if down("up"   ) then ball.body:setPosition(650/2, 650/2); ball.body:setLinearVelocity(0, 0) end 
+    
+    world:update(dt)
 end
 
 function love.draw()
-
     lg.setColor(0.28, 0.63, 0.05)
     lg.polygon("fill", ground.body:getWorldPoints(ground.shape:getPoints()))
     lg.polygon("fill", ground.body:getWorldPoints(ground.shape2:getPoints()))
@@ -53,5 +55,4 @@ function love.draw()
     
     lg.setColor(0.20, 0.20, 0.20)
     lg.polygon("fill", block.body:getWorldPoints(block.shape:getPoints()))
-
 end
