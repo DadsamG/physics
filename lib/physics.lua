3a8bf7551dfaa14d845ae2551a55582d1b56451e
4v0v <4vZEROv@gmail.com>
2019-01-05 00:15:35 +0100

    update

--- lib/physics.lua
+++ lib/physics.lua
@@ -8,6 +8,14 @@ local function _set_funcs(obj1, obj2)
     end
 end
 
+local function uuid()
+    local fn = function(x)
+        local r = math.random(16) - 1
+        r = (x == "x") and (r + 1) or (r % 4) + 9
+        return ("0123456789ABCDEF"):sub(r, r)
+    end
+    return (("xxxxxxxx"):gsub("[x]", fn))
+end
 
 local Collider = {}
 
@@ -16,8 +24,8 @@ function Collider:new(world, type, ...)
     if     type == "circ"  then body, shape = lp.newBody(world, _a[1], _a[2], _a[4] and _a[4].type or "dynamic"), lp.newCircleShape(_a[3])
     elseif type == "rect"  then body, shape = lp.newBody(world, _a[1], _a[2], _a[5] and _a[5].type or "dynamic"), lp.newRectangleShape(0, 0, _a[3], _a[4], _a[5] and _a[5].angle or 0)
     elseif type == "poly"  then body, shape = lp.newBody(world,     0,     0, _a[2] and _a[2].type or "dynamic"), lp.newPolygonShape(unpack(_a[1]))
-    elseif type == "line"  then body, shape = lp.newBody(world,     0,     0, _a[5] and _a[5].type or "dynamic"), lp.newEdgeShape(_a[1], _a[2], _a[3], _a[4])
-    elseif type == "chain" then body, shape = lp.newBody(world,     0,     0, _a[3] and _a[3].type or "dynamic"), lp.newChainShape(_a[1], unpack(_a[2])) end
+    elseif type == "line"  then body, shape = lp.newBody(world,     0,     0, _a[5] and _a[5].type or "static" ), lp.newEdgeShape(_a[1], _a[2], _a[3], _a[4])
+    elseif type == "chain" then body, shape = lp.newBody(world,     0,     0, _a[3] and _a[3].type or "static" ), lp.newChainShape(_a[1], unpack(_a[2])) end
 
     local obj = {}
         obj.world    = world
@@ -26,9 +34,8 @@ function Collider:new(world, type, ...)
         obj.fixture  = lp.newFixture(body, shape)
         obj.shapes   = {default = obj.shape}
         obj.fixtures = {default = obj.fixture}
-        obj.__index  = Collider
         _set_funcs(obj, obj.body);_set_funcs(obj, obj.shape);_set_funcs(obj, obj.fixture)
-    return setmetatable(obj, obj)
+    return setmetatable(obj, {__index = Collider})
 end
 
 function Collider:destroy()
@@ -38,7 +45,7 @@ function Collider:destroy()
 end
 
 function Collider:add_shape(type, name, ...)
-    local _a, shape = {...}
+    local _a, name, shape = {...}, name or uuid()
     if     type == "circle"    then shape = lp.newCircleShape(_a[1], _a[2], _a[3])
     elseif type == "rectangle" then shape = lp.newRectangleShape(_a[1], _a[2], _a[3], _a[4], _a[5])
     elseif type == "polygon"   then shape = lp.newPolygonShape(unpack(_a[1]))
@@ -53,24 +60,21 @@ end
 
 local World = {}
 
-function World:__call(x,y,z) 
+function World:__call(xg,yg,sleep) 
     local obj = {}
-        obj.box2d_world = lp.newWorld(x,y,z)
+        obj.box2d_world = lp.newWorld(xg,yg,sleep)
         _set_funcs(obj, obj.box2d_world)
     return setmetatable(obj, {__index = World})
 end
 
 function World:draw() 
-    for k1,v1 in pairs(self:getBodies()) do 
-        for k2, v2 in pairs(v1:getFixtures()) do 
-            if     v2:getShape():getType() == "circle"  then lg.circle("line", v1:getX(), v1:getY(), v2:getShape():getRadius())
-            elseif v2:getShape():getType() == "polygon" then lg.polygon("line", v1:getWorldPoints(v2:getShape():getPoints()))
-            else  
-                local points = {v1:getWorldPoints(v2:getShape():getPoints())}    
-                for i=1, #points, 2 do if i < #points-2 then lg.line(points[i], points[i+1], points[i+2], points[i+3]) end end
-            end
-        end
-    end
+    -- Joints --
+    -- Colliders --
+    for k1,v1 in pairs(self:getBodies()) do for k2, v2 in pairs(v1:getFixtures()) do 
+        if     v2:getShape():getType() == "circle"  then lg.circle("line", v1:getX(), v1:getY(), v2:getShape():getRadius())
+        elseif v2:getShape():getType() == "polygon" then lg.polygon("line", v1:getWorldPoints(v2:getShape():getPoints()))
+        else   local _p = {v1:getWorldPoints(v2:getShape():getPoints())}; for i=1, #_p, 2 do if i < #_p-2 then lg.line(_p[i], _p[i+1], _p[i+2], _p[i+3]) end end end
+    end end
 end
 
 function World:add_circle(x, y, r, args) return Collider:new(self.box2d_world, "circ", x, y, r, args) end
@@ -78,5 +82,19 @@ function World:add_rectangle(x, y, w, h, args) return Collider:new(self.box2d_wo
 function World:add_polygon(vertices, args) return Collider:new(self.box2d_world, "poly", vertices, args) end
 function World:add_line(x1, y1, x2, y2, args) return Collider:new(self.box2d_world, "line", x1, y1, x2, y2, args) end
 function World:add_chain(vertices, loop, args) return Collider:new(self.box2d_world, "chain", vertices, loop, args) end
+
+function World:add_joint(type, col1, col2, ...)
+    if     type == "distance"  then return lp.newDistanceJoint(col1.body, col2.body, ...)
+    elseif type == "friction"  then return lp.newFrictionJoint(col1.body, col2.body, ...)
+    elseif type == "gear"      then return lp.newGearJoint(col1.body, col2.body, ...)
+    elseif type == "motor"     then return lp.newMotorJoint(col1, col2, ...)
+    elseif type == "mouse"     then return lp.newMouseJoint(col1.body, col2.body, ...)
+    elseif type == "prismatic" then return lp.newPrismaticJoint(col1.body, col2.body, ...)
+    elseif type == "pulley"    then return lp.newPulleyJoint(col1.body, col2.body, ...)
+    elseif type == "revolute"  then return lp.newRevoluteJoint(col1.body, col2.body, ...)
+    elseif type == "rope"      then return lp.newRopeJoint(col1.body, col2.body, ...)
+    elseif type == "weld"      then return lp.newWeldJoint(col1.body, col2.body, ...)
+    elseif type == "wheel"     then return lp.newWheelJoint(col1.body, col2.body, ...) end
+end
 
 return setmetatable({}, World)
--- main.lua
+++ main.lua
@@ -4,7 +4,7 @@ require("run")
 World = require("lib/physics")
 
 
-world = World()
+local world = World()
 
 -- world:setCallbacks(
 --     function(a, b, coll) end, -- begin
@@ -14,44 +14,41 @@ world = World()
 -- )
 
 
-ball = world:add_circle(500, 500, 20)
+walls = world:add_chain(true, {1,1, 800,1, 800, 600, 1, 599})
+walls:setFriction(0)
+
+ball = world:add_circle(400, 400, 20)
 ball:setRestitution(1)
-ball2 = world:add_circle(200, 100, 20)
-ball3 = world:add_circle(300, 100, 20)
-ball4 = world:add_circle(400, 100, 20)
-ball5 = world:add_circle(500, 100, 20)
-ball6 = world:add_circle(600, 100, 20)
+ball:setFriction(0)
 
-wall1 = world:add_rectangle(400,   1, 800,   1, {type = "static"})
-wall2 = world:add_rectangle(400, 599, 800,   1, {type = "static"})
-wall3 = world:add_rectangle(1,   300,   1, 600, {type = "static"})
-wall4 = world:add_rectangle(799, 300,   1, 600, {type = "static"})
+bricks = {ox = 10, oy = 10}
+for i = 1, 5 do
+    for j = 1, 6 do
+        bricks[j] = world:add_rectangle(100 * j + bricks.ox * j-1, 30 * i + bricks.oy * i - 1, 100, 30, {type = "static"})
+        bricks[j]:setFriction(0)
+    end
+end
 
-rect  = world:add_rectangle(400, 400, 100, 200, {angle = 1})
-rect:add_shape("circle", "test", 0, 0, 100)
+pad = world:add_rectangle(500, 500, 100, 20)
+pad:setRestitution(0)
+pad:setLinearDamping(100)
+pad:setFriction(0)
+pad:setFixedRotation(true)
 
-line  = world:add_line(50, 50, 500, 10)
-line:setRestitution(1)
+lilnigga = world:add_rectangle(2,500, 10, 20, {type = "static"})
 
-chain = world:add_chain(false,{50, 50, 100, 100, 100, 160})
-chain:setRestitution(1)
-
-poly  = world:add_polygon({200,200, 200, -30, -30,300, 300,300, 300,200})
-poly:add_shape("rectangle", "test", 100, 100, 100, 200, 40)
-poly:add_shape("circle", "test2", 0, 0, 100)
-
+joint = world:add_joint("prismatic", lilnigga, pad, 0,0, 200, 0, false )
+joint:setLimitsEnabled(false)
 
 -------------------------------
 --  <°)))>< <°)))>< <°)))><  --
 -------------------------------
 
 function love.update(dt)
-    if down("right") then ball:applyForce( 1000,     0) end
-    if down("left" ) then ball:applyForce(-1000,     0) end
-    if down("up"   ) then ball:applyForce(    0, -1000) end
-    if down("down" ) then ball:applyForce(    0,  1000) end
+    if down("right") then pad:applyForce( 500000,     0)  end
+    if down("left" ) then pad:applyForce(-500000,     0)  end
+    if down("space" ) then ball:applyForce(-5000,     -5000)  end
 
-    
     world:update(dt)
 end
 
