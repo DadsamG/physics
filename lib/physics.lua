7c9805c18947b9c581dbe384cca95e4f637e032e
4v0v <4vZEROv@gmail.com>
2019-02-02 01:24:03 +0100

    update

--- lib/physics.lua
+++ lib/physics.lua
@@ -26,15 +26,15 @@ local function _callback(fix1, fix2, contact, coll_type, ...)
     local body1, body2   = fix1:getBody()     , fix2:getBody()
     local coll1, coll2   = body1:getUserData(), body2:getUserData()
     local shape1, shape2 = fix1:getUserData() , fix2:getUserData()
-    shape1[coll_type](coll2, shape2, coll1, shape1, contact, ...)
-    shape2[coll_type](coll1, shape1, coll2, shape2, contact, ...)
-    coll1[coll_type](coll2, shape2, coll1, shape1, contact, ...)
-    coll2[coll_type](coll1, shape1, coll2, shape2, contact, ...)
+    shape1[coll_type](coll1, shape1, coll2, shape2, contact, ...)
+    shape2[coll_type](coll2, shape2, coll1, shape1, contact, ...)
+    coll1[coll_type](coll1, shape1, coll2, shape2, contact, ...)
+    coll2[coll_type](coll2, shape2, coll1, shape1, contact, ...)
 end
-local function _enter(fix1, fix2, contact)     return _callback(fix1, fix2, contact,'_enter'    ) end
-local function _exit(fix1, fix2, contact)      return _callback(fix1, fix2, contact,'_exit'     ) end
-local function _pre(fix1, fix2, contact)       return _callback(fix1, fix2, contact,'_pre'      ) end
-local function _post(fix1, fix2, contact, ...) return _callback(fix1, fix2, contact,'_post', ...) end -- ... => normal_impulse1, tangent_impulse1, normal_impulse2, tangent_impulse2
+local function _enter(fix1, fix2, contact)     return _callback(fix1, fix2, contact,"_enter")     end
+local function _exit(fix1, fix2, contact)      return _callback(fix1, fix2, contact,"_exit")      end
+local function _pre(fix1, fix2, contact)       return _callback(fix1, fix2, contact,"_pre")       end
+local function _post(fix1, fix2, contact, ...) return _callback(fix1, fix2, contact,"_post", ...) end -- ... => normal_impulse1, tangent_impulse1, normal_impulse2, tangent_impulse2
 
 -------------------------------
 --  <°)))>< <°)))>< <°)))><  --
--- main.lua
+++ main.lua
@@ -8,15 +8,17 @@ World = require("lib/physics")
 local world = World(0, 0)
 
 world:add_chain("walls", true, {1,1, 800,1, 800, 600, 1, 599}, "static")
-world:get_c("walls"):setFriction(0)
+world:get_c("walls"):setFriction(1)
 
 world:add_circle("ball", 400, 400, 20)
-world:get_c("ball"):setRestitution(1)
-world:get_c("ball"):setFriction(0)
+world:get_c("ball"):setFriction(1)
 world:get_c("ball"):add_rectangle("rect", 0, 0, 10, 100, 0)
+world:get_c("ball"):setFriction(1)
 
-world:get_s("ball", "rect"):set_exit(function(collider1, shape1, collider2, shape2, contact) print(contact) end)
-world:get_s("ball", "rect"):set_post(function(collider1, shape1, collider2, shape2, contact, ni1, ti1, ni2, ti2) end)
+world:get_c("ball"):set_enter(function(c1, s1, c2, s2, contact) end)
+world:get_c("ball"):set_exit(function(c1, s1, c2, s2, contact) end)
+world:get_c("ball"):set_pre(function(c1, s1, c2, s2, contact) end)
+world:get_c("ball"):set_post(function(c1, s1, c2, s2, contact, ni1, ti1, ni2, ti2) end)
 
 -------------------------------
 --  <°)))>< <°)))>< <°)))><  --
